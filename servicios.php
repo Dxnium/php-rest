e561d61f563ae69f548435c3df3fec23dd64a563
Dxnium <38996231+Dxnium@users.noreply.github.com>
2023-11-28 15:36:21 -0600

    Servicios (#4)
    * Pequeños cambios
    
    * Mi parte
    
    * servicios.php updates
    
    ---------
    
    Co-authored-by: Santiago Blanco Rentería <134663445+santiblanco01@users.noreply.github.com>

--- servicios.php
+++ servicios.php
@@ -1,6 +1,17 @@
 <?php include "header.php" ?>
 
-<h1>Servicios</h1>
-
+<main class="container">
+    <section class= "row mt-3">
+    <h1>Servicios que ofrecemos</h1>
+    </section>
+    <section class="row mt-4">
+        <h2>Sevicio a la americana</h2>
+        <p class="col-md-6">Preparamos nuestros deliciosos alimentos en la cocina con mucho higiene y amor, y uno de nuestros camareros le servirá su plato de comida en su mesa correspondiente</p>
+    </section>
+    <section class="">
+        <h2>Servicio al cliente</h2>
+        <p class="col-md-6">Aqui en ColoDelicias el cliente siempre tiene la razón. La principal meta de nuestro servicio es asegurar la satisfacción de sus necesidades</p>
+    </section>
+</main>
 
 <?php include "footer.php" ?>
