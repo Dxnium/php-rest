d9a0e65186e6ebb3e2945e912fa0a689b22847ac
Santiago Blanco Rentería <134663445+santiblanco01@users.noreply.github.com>
2023-12-06 21:27:49 -0600

    menu changes

--- footer.php
+++ footer.php
@@ -30,10 +30,15 @@
 </footer>
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous"></script>
 <script src="./js/script.js"></script>
 
+
+
+</body>
+
+</html>
 
 
 </body>
