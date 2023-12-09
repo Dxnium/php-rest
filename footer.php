630bcc9e0e46737037be95240f7f0415d920c365
Santiago Blanco Rentería <134663445+santiblanco01@users.noreply.github.com>
2023-12-09 15:58:46 -0600

    Menu,-header,-footer,-tema-y-img

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer text-center">
+<footer class="footer text-center bg-amarillo text-black">
     <div class="container">
         <div class="row">
             <!-- Footer Location-->
@@ -7,16 +7,15 @@
                 <p class="lead mb-0">
                     San Jose, Costa Rica
                     <br />
-                    Mall San Pedro
+                    Barrio Escalante
                 </p>
             </div>
             <!-- Footer Social Icons-->
             <div class="col-lg-4 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Redes Sociales</h4>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                <a class="btn btn-primary btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                <a class="btn btn-primary btn-social mx-1" href="#!"><i class="fa-brands fa-x-twitter"></i></a>
+                <a class="btn btn-primary btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
             </div>
             <!-- Footer About Text-->
             <div class="col-lg-4">
@@ -30,23 +29,10 @@
 </footer>
 
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
-
-
-<script src="https://kit.fontawesome.com/aa284dc2ce.js" crossorigin="anonymous"></script>
-<script src="js/vendor/modernizr-3.11.2.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
 <script src="./js/script.js"></script>
-<script src="./js/login.js"></script>
 
-
-
-</body>
-
-</html>
 
 
 </body>
