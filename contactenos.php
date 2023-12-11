1e7cb5e7a6fbd9d4b488d53f3a4f2587628ed570
Dxnium <38996231+Dxnium@users.noreply.github.com>
2023-12-11 09:34:35 -0600

    unicos cambios Dany (#12)

--- contactenos.php
+++ contactenos.php
@@ -2,7 +2,7 @@
 
 <main class="container">
     <section class="row mt-3 pb-3">
-        <form class="col-lg-6" action="">
+        <form class="col-lg-6" action="https://formspree.io/f/mjvqnpen" method="POST">
             <div class="form-floating mb-3">
                 <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Digite su Nombre">
                 <label for="nombre">Nombre</label>
@@ -27,6 +27,9 @@
                 
         </form>
         <iframe class="col-lg-6" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d503024.8031565235!2d-84.48872054926868!3d9.94383530000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb86ef090a93%3A0xf2ae13c3b922006c!2sCinemark%20%E2%80%A2%20Multiplaza%20Escaz%C3%BA!5e0!3m2!1ses-419!2scr!4v1696389330757!5m2!1ses-419!2scr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="col-lg-6" src="https://maps.app.goo.gl/ErW5JPKtCCEowqWC8" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="col-lg-6" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d503024.8031565235!2d-84.48872054926868!3d9.94383530000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0fb86ef090a93%3A0xf2ae13c3b922006c!2sCinemark%20%E2%80%A2%20Multiplaza%20Escaz%C3%BA!5e0!3m2!1ses-419!2scr!4v1696389330757!5m2!1ses-419!2scr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
     </section>
 </main>
 
--- header.php
+++ header.php
@@ -12,6 +12,13 @@
     <meta property="og:url" content="">
     <meta property="og:image" content="">
 
+    <!-- Agrega este enlace a tu archivo header.php -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
 
     <link rel="apple-touch-icon" href="icon.png">
     <!-- Place favicon.ico in the root directory -->
@@ -37,6 +44,7 @@
             </button>
             <div class="collapse navbar-collapse" data-bs-target="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="principal.php">Principal</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="menu.php">Menú</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="servicios.php">Servicios</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="acerca.php">Acerca de Nosotros</a></li>
