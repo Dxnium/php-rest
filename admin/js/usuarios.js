0ac6c9c8ce24ee4fdf7de8f412db3859cb65c890
Dxnium <38996231+Dxnium@users.noreply.github.com>
2023-12-12 17:21:04 -0600

    borrado usuario (#20)

--- admin/js/usuarios.js
+++ admin/js/usuarios.js
@@ -24,7 +24,6 @@ function cargarTabla() {
                     <td scope="col">${u.fecha}</td>
                     <td scope="col">${u.telefono}</td>
                     <td scope="col">
-                        <button class=" me-3 btn btn-outline-warning"><i class="fa-solid fa-pen-to-square"></i></button>
                         <button class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button>
                     </td>
                 </tr>
@@ -47,3 +46,70 @@ function cargarTabla() {
 document.querySelector("#perfil-link").addEventListener("click", (e) => {
     location.href = "index.php?page=perfil"
 })
+
+
+//crear eventos a nodos dinamicos
+//definir un eventlister al padre de los nodos dinamicos
+document.querySelector("#datosTabla").addEventListener("click", (e) => {
+  //discriminar los nodos a aplicar el evento
+ if (e.target.classList.contains("btn-outline-danger") ||
+    e.target.classList.contains("fa-trash")) {
+    eliminarUser(e.target.closest("tr").childNodes[1].innerHTML)
+  }
+})
+
+
+function eliminarUser(id) {
+  //console.log(id)
+  Swal.fire({
+    title: "¿Esta seguro que desea eliminar este Registro?",
+    text: "Los datos no se podrán recuperar",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Si, Borrar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if (procesarBorradoUser(id)) {
+        Swal.fire({
+          title: "¡Mensaje!",
+          text: "Datos Borrados correctamente",
+          icon: "success"
+        });
+      }else{
+        Swal.fire({
+          title: "¡Mensaje!",
+          text: "Error al procesar la petición",
+          icon: "error"
+        });
+      }
+    }
+  });
+}
+
+function procesarBorradoUser(id) {
+  resultado = 1
+  console.log(id)
+  $.ajax({
+    type: "delete",
+    url: `http://localhost/php-rest/apirest/src/public/delusuario/${id}`,
+    beforeSend: function () {
+      //document.querySelector(".cargando").style.visibility="visible";
+
+    },
+    success: function (res) {
+      console.log(res)
+      result = 1;
+    },
+    error: function (xhr) {
+      console.log("Error al procesar la petición");
+      console.log(xhr.statusText + xhr.responseText);
+
+    },
+    complete: function () {
+      //document.querySelector(".cargando").style.visibility="hidden";
+      return resultado
+    }
+  });
+}
